8535530de47d9f833d1b6c242d94a2d3584c98a4
Satyavrat <gaur.satyavrat1995@gmail.com>
2018-11-28 21:47:14 -0700

    cleaned repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# pvrl
-Code for Web interface in Python Django backend and HTML/CSS for UI design
--- pvrl-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Library/Frameworks/Python.framework/Versions/3.7/bin/PVRL/pvrl-env/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
